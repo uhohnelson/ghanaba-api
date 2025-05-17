48b8f169c2506bb8ca761de0a75ecc7ab6a8dc5b
uhohnelson <nelsonfreshwood@gmail.com>
2025-05-17 23:06:59 +0000

    Add homepage route

--- app.py
+++ app.py
@@ -19,3 +19,7 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+
+@app.route('/')
+def home():
+    return "Welcome to Ghanaba API. It's alive!"
